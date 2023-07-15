c4ac458d4f754835a37d00ed00dccbbace3104d3
0826280801 <mmawashap@gmail.com>
2023-07-15 12:28:45 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#Include <Stdio.'h>
+/*
+ * main - A program that prints a line with puts function
+ * return: 0 (Success)
+ */
+int main (void)
+{
+	puts ("\" Programming is like building a multilingual puzzle");
+	Return(0);
+}
